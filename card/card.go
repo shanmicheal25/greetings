4a93646ca87ca79e6a8f47a13bfe0083a374bad6
shankar <shanmicheal25@gmail.com>
2022-01-29 15:41:44 +0800

    adde adf adf

--- card/card.go
+++ card/card.go
@@ -15,3 +15,10 @@ func Marriage(name string) string {
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
 }
+
+// Hello returns a greeting for the named person.
+func Engagement(boy string, girl string) string {
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome to the Engagement boy , Are you happy to marry me %v", boy, girl)
+	return message
+}
