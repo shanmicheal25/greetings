3a1b72a08daecb5a6cfff8b47c9c561402676f2c
shankar <shanmicheal25@gmail.com>
2022-01-29 15:37:48 +0800

    test

--- card/card.go
+++ card/card.go
@@ -2,6 +2,13 @@ package card
 
 import "fmt"
 
+/*
+ * init function added
+ */
+func init() {
+	fmt.Println("Simple interest package initialized")
+}
+
 // Hello returns a greeting for the named person.
 func Marriage(name string) string {
 	// Return a greeting that embeds the name in a message.
